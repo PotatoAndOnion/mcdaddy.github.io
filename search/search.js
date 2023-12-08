255959a22e0cb410c2e70b909da6b9d09233b7dc
McDaddy <133293111+PotatoAndOnion@users.noreply.github.com>
2023-12-08 21:48:33 +0700

    update

--- search/search.js
+++ search/search.js
@@ -5,3 +5,11 @@ console.log(nike_data.response.results[0].value)
 import {adidas_data} from '../data/adidasdata.js'
 console.log(adidas_data)
 
+import {converse_data} from '../data/conversedata.js'
+console.log(converse_data)
+
+import {vans_data} from '../data/vansdata.js'
+console.log(vans_data)
+
+import {nb_data} from '../data/nbdata.js'
+console.log(nb_data)
